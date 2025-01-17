a3f492c2891303bf103ceb863e1c8907a71a23e5
alcedok <kevinalcedo@gmail.com>
2025-01-17 14:53:56 +0000

    Integration of HMM implementation (#1)
    * Initial version
    
    ---------
    
    Co-authored-by: Dinis Miranda <dinis.miranda@tecnico.ulisboa.pt>

--- localization.py
+++ localization.py
@@ -15,6 +15,40 @@ def init_probabilities(num_states):
 def get_state_probabilities(observation, transition_matrix, observation_matrix, observation_history=[]):
     '''
     Localization must return a numpy array of shape (Z,) corresponding to the probability distribution over states
+    
+    Computes state probabilities using Hidden Markov Model (HMM) for localization.
+    
+    Parameters:
+        observation: numpy array of shape (4,), the current observation
+        transition_matrix: numpy array of shape (N*N, N*N), transition probabilities
+        observation_matrix: numpy array of shape (N*N, Z), observation probabilities
+        observation_history: list of observations (numpy arrays)
+    
+    Returns:
+        numpy array of shape (N*N,) representing state probabilities
     '''
-    #TODO: needs to be implemented 
-    return None
+    N = int(np.sqrt(transition_matrix.shape[0]))  # Extract grid size (N x N)
+    num_states = N * N  # Total number of states
+    num_observations = observation_matrix.shape[1]  # Total number of unique observations (Z)
+    
+    # Initial uniform belief if there's no history
+    belief = np.full(num_states, 1 / num_states)
+    
+    # Process the observation history
+    for obs in observation_history:
+        # Prediction step
+        belief = np.dot(transition_matrix.T, belief)
+        
+        # Correction step for historical observations
+        obs_index = int("".join(map(str, obs)), 2)  # Convert binary vector to integer index
+        belief = observation_matrix[:, obs_index] * belief
+        
+        # Normalize the belief
+        belief /= np.sum(belief)
+
+    # Incorporate the latest observation
+    obs_index = int("".join(map(str, observation)), 2)  # Convert binary vector to integer index
+    belief = observation_matrix[:, obs_index] * belief
+    belief /= np.sum(belief)  # Normalize again
+
+    return belief  # Return as a flat array of shape (N*N,)
